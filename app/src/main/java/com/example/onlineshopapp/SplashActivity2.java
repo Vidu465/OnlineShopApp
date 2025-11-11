b8817dbc806b0e0f3a77a4c6823095ddd7b99cd0
Vihara <viharavidu465@gmail.com>
2025-11-11 17:59:51 +0530

    Added buttons

--- app/src/main/java/com/example/onlineshopapp/SplashActivity2.java
+++ app/src/main/java/com/example/onlineshopapp/SplashActivity2.java
@@ -1,6 +1,8 @@
 package com.example.onlineshopapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.onlineshopapp.databinding.ActivitySplash2Binding;
+
 public class SplashActivity2 extends AppCompatActivity {
+    private ActivitySplash2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding=ActivitySplash2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.setOnClickListener(v ->
+                startActivity(new Intent(SplashActivity2.this,MainActivity.class)));
+
+
     }
 }
