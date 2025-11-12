8b919d7d19d15dcb3f04fca26978497e2f1e84be
Vihara <viharavidu465@gmail.com>
2025-11-12 09:56:29 +0530

    Added categories to the dashboard

--- app/src/main/java/com/example/onlineshopapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/onlineshopapp/Activity/MainActivity.java
@@ -1,26 +1,40 @@
 package com.example.onlineshopapp.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.onlineshopapp.R;
+import com.example.onlineshopapp.ViewModel.MainViewModel;
+import com.example.onlineshopapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initCateggory();
+
+    }
+
+    private void initCateggory() {
+        binding.progressBar2Category.setVisibility(View.VISIBLE);
+        viewModel.LoadCategory().observeForever(categoryModels -> {
+            binding.categoryView.setLayoutManager(new LinearLayoutManager(
+                    MainActivity.this,LinearLayoutManager.HORIZONTAL,false));
         });
     }
+
 }
