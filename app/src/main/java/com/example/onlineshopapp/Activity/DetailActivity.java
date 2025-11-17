599dbfff0cfced6b44a4641d508f879ad2a5b572
Vihara <viharavidu465@gmail.com>
2025-11-17 09:57:28 +0530

    Added items

--- app/src/main/java/com/example/onlineshopapp/Activity/DetailActivity.java
+++ app/src/main/java/com/example/onlineshopapp/Activity/DetailActivity.java
@@ -8,19 +8,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.onlineshopapp.Domain.ItemsModel;
+import com.example.onlineshopapp.Helper.ManagmentCart;
 import com.example.onlineshopapp.R;
+import com.example.onlineshopapp.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
-
+private ActivityDetailBinding binding;
+private ItemsModel object;
+private int numberOrder=1;
+private ManagmentCart managmentCart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding=ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        managmentCart=new ManagmentCart(this);
+
+        getBundle();
+
+
+    }
+
+    private void getBundle() {
+        object=get
     }
 }
